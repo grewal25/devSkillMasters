8e66aa602c61f27c31ea897e40568a518bcd3ea6
Saranjeet Singh <saran@Saranjeets-MacBook-Pro.local>
2023-06-05 14:19:02 -0400

    rename project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
 import { Roboto } from "next/font/google";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
-import Middle from "./components/middle";
-import Main from "./components/main";
 import Outline from "./components/outline";
-import Carousel from "./components/carousel";
 import Curriculum from "./components/curriculum";
 import Price from "./components/price";
-import CarouselDefault from "./components/carousel-default";
 import FandQ from "./components/fandq";
 import Footer from "./components/footer";
 
@@ -22,15 +17,11 @@ export default function Home() {
       <main className="flex flex-col justify-start px-8">
         <Navbar />
         <Hero />
-        {/* <Main /> */}
         <Outline />
-        {/* <CarouselDefault /> */}
-        {/* <Carousel /> */}
         <Curriculum />
         <Price />
         <FandQ />
         <Footer />
-        {/* <Middle /> */}
       </main>
     </div>
   );
